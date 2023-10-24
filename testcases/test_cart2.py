8450942fa8cc8e23565b99be3cb7bc581c41aefa
Tony <uyahanthony008@gmail.com>
2023-10-24 23:55:47 +0100

    initial commit

--- testcases/test_cart2.py
+++ testcases/test_cart2.py
@@ -12,8 +12,8 @@ import requests
 
 class Test_cart_002:
     URL = "https://magento.softwaretestingboard.com"
-    EXISTING_EMAIL = "ubongphilip2200@gmail.com"
-    EXISTING_PASSWORD = "Ubong123?"
+    EXISTING_EMAIL = "dfghjkdfghjk@gmail.com"
+    EXISTING_PASSWORD = "@ynR!TF6Xki8XWr"
     logger = recordLogger.log_generator_info()
 
     def log_test_start(self, test_name):
@@ -37,8 +37,8 @@ class Test_cart_002:
         self.AO = accountCreationObjects(self.driver)
         # Log in and go to the welcome page
         self.AO.path_to_signinButton()
-        self.AO.emailSignin("ubongphilip2200@gmail.com")
-        self.AO.passwordSignin("Ubong123?")
+        self.AO.emailSignin(self.EXISTING_EMAIL)
+        self.AO.passwordSignin(self.EXISTING_PASSWORD)
         self.AO.signinButton()
         # navigate to the men url for item selection
         self.MC = MenPageObject(self.driver)
@@ -49,31 +49,26 @@ class Test_cart_002:
         time.sleep(2)
         self.MC = MenPageObject(self.driver)
         # Define the locators for the items and pick 4 items and add to the cart
-        ItemsOfHoodiesPage = self.MC.addHoodies_sweatshirtToCart()           # take you to the catalog for the hoodies
+        ItemsOfHoodiesPage = self.MC.addHoodies_sweatshirtToCart2()           # take you to the catalog for the hoodies
         ListOfAddedItems = []
         time.sleep(2)
 
         # defining the sizes
         sizes = ['XS', 'S', 'M', 'L', 'XL']         # INDEX ERROR
         numbers = [1, 2, 3]
+        wait = WebDriverWait(self.driver, 10)
 
-        for items in range(0, len(ItemsOfHoodiesPage), 1):
-            print(ItemsOfHoodiesPage[items])
-            time.sleep(2)
-            # Check if the current index is within the valid range
-            if items < len(ItemsOfHoodiesPage):
-                # ListOfAddedItems.append(ItemsOfHoodiesPage[items].get_attribute("text"))  ## NO TEXT ATTRIBUTE
-                # print(ListOfAddedItems)
-                time.sleep(4)
-                ItemsOfHoodiesPage[items].click()
-                # getting a random size for the item
-                size = random.choice(sizes)
-                number = random.choice(numbers)
-                self.MC.sizePicker(size)
+        for xpath in ItemsOfHoodiesPage:
+            element = wait.until(EC.element_to_be_clickable((By.XPATH, xpath)))
+            ListOfAddedItems.append(element.get_attribute("text"))
+            element.click()
 
-                self.MC.colorPicker(number)
-            else:
-                break  # Exit the loop if the index is out of range
+            # getting a random size for the item
+            size, number = random.choice(sizes), random.choice(numbers)
+
+            self.MC.sizePicker(size)
+
+            self.MC.colorPicker(number)
 
             self.MC.addToCart()
             time.sleep(3)
@@ -81,9 +76,11 @@ class Test_cart_002:
 
         # call on the items in the cart and verify the correct addition of the items tot eh cart
         CartItems = self.MC.cart_items()
+        cleanedList = [item.strip() for item in ListOfAddedItems]
+        print(CartItems,cleanedList)
         time.sleep(2)
 
-        assert set(ListOfAddedItems).issubset(set(CartItems)), self.logger.info("***** TEST FAILED: ITEMS NOT ADDED TO THE CART ****")
+        assert set(cleanedList).__eq__ (set(CartItems)), self.logger.info("***** TEST FAILED: ITEMS NOT ADDED TO THE CART ****")
 
         self.logger.info("**** TEST SUCCESSFUL: ITEMS WERE ADDED TO THE CART *******")
 
